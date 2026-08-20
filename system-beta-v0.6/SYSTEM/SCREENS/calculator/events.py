f7580319293a561470a820c392545b1508237cb7
Sam <samuelgomesdf1@gmail.com>
2026-08-20 20:15:03 -0300

    Add files via upload

---
+++ system-beta-v0.6/SYSTEM/SCREENS/calculator/events.py
@@ -0,0 +1,62 @@
+from SYSTEM.SCREENS.calculator import ui
+from SYSTEM.DISPLAY import renderer
+from time import sleep
+def igual():
+    texto = ""
+    for elmt in ui.UI:
+        if elmt["action"] == "Typ":
+            #print(elmt)
+            for txt in elmt["text"]:
+                #print(elmt["text"])
+                #print(0)
+                texto += str(txt)
+            try:
+                #print(1)
+                print(texto)
+               # elmt["text"] = [""]
+               # elmt["lin"] = 0
+                #print(3)
+                texto = str(eval(texto))
+               #print(elmt["text"])
+            except Exception:
+                texto = "Erro"
+            texto_ui = elmt
+            
+        elif elmt["type"] == "rect" and elmt["action"] == "Typing":
+            surfice = elmt
+    #print(4)
+    quart_surf = int(surfice["layout"]["size"][0] * 0.75)
+    #quart_surf = int(surf * 0.75)
+    text_lin = []
+    chr_surf = quart_surf // 8
+    size_text = len(texto)*8
+    #print(texto)
+    #print(quart_surf)
+    #print(size_text)
+    while True:
+        size_text = len(texto) * 8
+        
+        if size_text <= quart_surf:
+            #print("saiu")
+            text_lin.append(texto)
+            break
+        
+        elif size_text > quart_surf:
+            text_lin.append(texto[:chr_surf])
+            texto = texto[chr_surf:]
+            
+        size_text = len(texto) * 8
+        #print(size_text)
+        
+    #print(text_lin)
+    #print("Barra")
+    #print(texto)
+    texto_ui["text"] = text_lin
+    texto_ui["lin"] = len(text_lin)-1
+    texto_ui["layout"]["pos"] = [[0,i*16] for i in range(len(text_lin))]
+    print(texto_ui["lin"])
+            
+            
+    renderer.render([surfice])
+    renderer.render([texto_ui],True)
+        
