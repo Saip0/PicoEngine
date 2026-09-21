e12d27a4f3b5b641c81561ac2e5766d3677ed5ac
Sam <samuelgomesdf1@gmail.com>
2026-09-21 18:23:42 -0300

    Add files via upload

---
+++ system-beta-v0.7.2/SYSTEM/SCREENS/lock_screen/substates/home/substates/notepad/substates/create_not/pross_events.py
@@ -0,0 +1,3 @@
+from SYSTEM.SCREENS.lock_screen.substates.home.substates.notepad.substates.create_not import events
+def create():
+    events.criar()
---
+++ system-beta-v0.7.2/SYSTEM/SCREENS/lock_screen/substates/home/substates/notepad/ui.py
@@ -0,0 +1,7 @@
+from SYSTEM.DISPLAY.txt_ui import text_ui
+
+UI = [
+    text_ui(text="CRIAR",
+            pos=(0, 0),
+            font="16x16",
+            action="Create_Note"),]
