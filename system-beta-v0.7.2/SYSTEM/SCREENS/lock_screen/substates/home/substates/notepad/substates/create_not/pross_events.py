f1bb1c5155d1c175017b87561a911f81950747e9
Sam <samuelgomesdf1@gmail.com>
2026-09-23 21:01:30 -0300

    Delete system-beta-v0.7.2 directory

--- system-beta-v0.7.2/SYSTEM/SCREENS/lock_screen/substates/home/substates/notepad/substates/create_not/pross_events.py
+++
@@ -1,3 +0,0 @@
-from SYSTEM.SCREENS.lock_screen.substates.home.substates.notepad.substates.create_not import events
-def create():
-    events.criar()
--- system-beta-v0.7.2/SYSTEM/STORAGE/storage.py
+++
@@ -1,7 +0,0 @@
-sd = None
-def init():
-    global sd
-    from libs import init as Init
-    import os
-    sd = Init.sdcard()
-    os.mount(sd, "/sd")
